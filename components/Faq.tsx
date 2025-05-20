ed66ebcc272d49815cc638d33b25ecaefacb1822
itseghosime-codebase <info.itseghosime@gmail.com>
2025-05-20 02:20:07 -0700

    faq

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -12,7 +12,7 @@ import { BlurFade } from "./magicui/blur-fade";
 
 export default function Faq() {
   return (
-    <div id="faq" className="py-10">
+    <div id="faq" className="py-10 relative z-20">
       <div className="container">
         <BlurFade inView>
           <h4 className="text-center text-2xl md:text-3xl font-semibold text-primary pb-5">
@@ -79,21 +79,21 @@ const FaQuestions = [
   {
     heading: "What exactly do your DM setters do?",
     description:
-      " Yes. It comes with default styles that matches the other components&apos; aesthetic.",
+      "Our trained DM setters engage with your leads through direct messages (Instagram, Facebook, LinkedIn, etc.), spark real conversations, qualify prospects, and book them directly into your calendar for sales calls. They handle all the back-and-forth so you can focus on closing.",
   },
   {
-    heading: "What exactly do your DM setters do?",
+    heading: "Who trains your setters?",
     description:
-      " Yes. It comes with default styles that matches the other components&apos; aesthetic.",
+      "All our setters go through a proven training program led by experts in high-ticket sales and conversational marketing. They're taught to understand buyer psychology, objection handling, tonality, and how to represent your offer with clarity and confidence.",
   },
   {
-    heading: "What exactly do your DM setters do?",
+    heading: "Will the setters sound like me or my brand?",
     description:
-      " Yes. It comes with default styles that matches the other components&apos; aesthetic.",
+      "Yes. Before we begin, we work closely with you to understand your brand voice, target audience, and offer. We then create custom scripts and messaging frameworks to ensure every message feels authentic and aligned with your tone.",
   },
   {
-    heading: "What exactly do your DM setters do?",
+    heading: "What platforms do you operate on?",
     description:
-      " Yes. It comes with default styles that matches the other components&apos; aesthetic.",
+      "We currently support DM outreach on Instagram, Facebook, and LinkedIn. If you're active on multiple platforms, we’ll help you prioritize based on where your ideal clients are most engaged.",
   },
 ];
